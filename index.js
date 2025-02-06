38f095c3fceacdc03040e17e2781387376db4d03
elsinne47 <mushihli@gmail.com>
2025-02-06 22:52:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,36 @@ const { WebSocketServer } = require('ws');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const server = app.listen(process.env.PORT || 3001);
+const wss = new WebSocketServer({ 
+  server,
+  path: "/ws" // Add this line
 });
 
-const wss = new WebSocketServer({ server });
-
-let votes = {
-  jury: {},
-  peer: {}
-};
+let votes = { jury: {}, peer: {} };
 
 wss.on('connection', (ws) => {
+  console.log('Client connected');
   ws.send(JSON.stringify({ type: 'votes', data: votes }));
 
   ws.on('message', (message) => {
-    const data = JSON.parse(message);
-    if (data.type === 'vote') {
-      votes = data.votes;
-      wss.clients.forEach(client => {
-        client.send(JSON.stringify({ type: 'votes', data: votes }));
-      });
-    } else if (data.type === 'reset') {
-      votes = { jury: {}, peer: {} };
-      wss.clients.forEach(client => {
-        client.send(JSON.stringify({ type: 'votes', data: votes }));
-      });
+    try {
+      const data = JSON.parse(message);
+      if (data.type === 'vote') {
+        votes = data.votes;
+        wss.clients.forEach(client => {
+          client.send(JSON.stringify({ type: 'votes', data: votes }));
+        });
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
   });
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
