7d645a3a958668eebd9e79502cf0c7b5aa29163a
elsinne47 <mushihli@gmail.com>
2025-02-07 14:55:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,42 +6,67 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen(process.env.PORT || 3001);
-const wss = new WebSocketServer({ 
-  server,
-  path: "/ws" // Add this line
+let votes = { jury: {}, peer: {} };
+
+// Regular HTTP endpoint
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
 
-let votes = { jury: {}, peer: {} };
+// Get current votes
+app.get('/votes', (req, res) => {
+  res.json(votes);
+});
+
+// Update votes
+app.post('/vote', (req, res) => {
+  const { voterType, voterName, selectedTeam } = req.body;
+  if (!votes[voterType]) {
+    votes[voterType] = {};
+  }
+  votes[voterType][voterName] = selectedTeam;
+  res.json(votes);
+});
+
+// Reset votes
+app.post('/reset', (req, res) => {
+  votes = { jury: {}, peer: {} };
+  res.json(votes);
+});
+
+const server = app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running on port ${process.env.PORT || 3001}`);
+});
+
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  
+  // Send current votes to new client
   ws.send(JSON.stringify({ type: 'votes', data: votes }));
 
   ws.on('message', (message) => {
     try {
+      console.log('Received:', message.toString());
       const data = JSON.parse(message);
+      
       if (data.type === 'vote') {
         votes = data.votes;
+        // Broadcast to all clients
+        wss.clients.forEach(client => {
+          client.send(JSON.stringify({ type: 'votes', data: votes }));
+        });
+      } else if (data.type === 'reset') {
+        votes = { jury: {}, peer: {} };
+        // Broadcast reset
         wss.clients.forEach(client => {
           client.send(JSON.stringify({ type: 'votes', data: votes }));
         });
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error processing message:', error);
     }
-  });
-});
-
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
-wss.on('connection', (ws) => {
-  console.log('New client connected');
-  
-  ws.on('message', (message) => {
-    console.log('Received message:', message.toString());
   });
 
   ws.on('close', () => {
